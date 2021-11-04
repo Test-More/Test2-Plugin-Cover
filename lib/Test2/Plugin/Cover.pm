5b1d169d80bfe73e6a1c875f6d2bde34f8af5e54
Chad Granum <exodist7@gmail.com>
2021-11-04 14:18:08 -0700

    Automated Version Bump

--- lib/Test2/Plugin/Cover.pm
+++ lib/Test2/Plugin/Cover.pm
@@ -10,7 +10,7 @@ use File::Spec();
 
 my $SEP = File::Spec->catfile('', '');
 
-our $VERSION = '0.000027';
+our $VERSION = '0.000028';
 
 # Directly modifying this is a bad idea, but for the XS to work it needs to be
 # a package var, not a lexical.
